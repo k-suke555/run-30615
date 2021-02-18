2d612e096b555256b16688b662aa3a761b6778e2
k-suke555 <keisuke.k.1991@gmail.com>
2021-02-18 19:49:26 +0900

    アソシエーションの追加によるコントローラーの追記

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -42,7 +42,7 @@ class ParksController < ApplicationController
 
   private
   def park_params
-    params.require(:park).permit(:name, :address, :image, :latitude, :longitude)
+    params.require(:park).permit(:name, :address, :image, :latitude, :longitude).merge(user_id: current_user.id)
   end
 
   def set_parks
