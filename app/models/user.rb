6b42f72671ed8d66358d4eb5e85682d31e7a7408
k-suke555 <keisuke.k.1991@gmail.com>
2021-02-21 15:09:29 +0900

    コメントテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :parks
+  has_many :comments
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A[a-z0-9]+\z/i }
